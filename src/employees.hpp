72e8ff30cab842c9dc94d3316e305fab2a7442af
Austin Bachurski <austinbachurski@gmail.com>
2026-01-20 22:49:31 -0700

    Add std::formatter for derived classes.

--- src/employees.hpp
+++ src/employees.hpp
@@ -8,6 +8,7 @@
 #ifndef EMPLOYEE_BASE_HPP
 #define EMPLOYEE_BASE_HPP
 
+#include <format>
 #include <string>
 #include <string_view>
 
@@ -17,13 +18,19 @@ class Employee
 public:
     virtual ~Employee() = default;
 
+    struct EmployeeBuilder
+    {
+        std::string_view name;
+        std::string_view password;
+    };
+
     std::string_view getName() { return name; }
     bool isCorrectPassword(std::string_view userPassword) { return userPassword == password; }
 
 protected:
-    Employee(std::string_view employeeName, std::string_view employeePassword)
-    : name{ employeeName }
-    , password{ employeePassword }
+    Employee(EmployeeBuilder const &params)
+    : name{ params.name }
+    , password{ params.password }
     {}
 
 private:
@@ -31,12 +38,90 @@ private:
     std::string password;
 };
 
-class HumanResources : public Employee
+class GeneralEmployee : public Employee
 {
 public:
-    HumanResources(std::string_view employeeName, std::string_view employeePassword)
-    : Employee(employeeName, employeePassword)
+    GeneralEmployee(EmployeeBuilder const &params)
+    : Employee(params)
     {}
+};
+
+class HumanResourcesEmployee : public Employee
+{
+public:
+    HumanResources(EmployeeBuilder const &params)
+    : Employee(params)
+    {}
+};
+
+class ManagerEmployee : public Employee
+{
+public:
+    ManagerEmployee(EmployeeBuilder const &params)
+    : Employee(params)
+    {}
+};
+
+template<>
+struct std::formatter<GeneralEmployee>
+{
+    constexpr auto parse(std::format_parse_context &context) const
+    {
+        return context.begin();
+    }
+    
+    auto format(GeneralEmployee const &employee, std::format_context &context) const
+    {
+        return std::format_to(context.out(),
+                "\nLogged in as General Employee"
+                "Please make a selection:\n"
+                "0. Exit\n"
+                "1. View employee file.\n"
+                );
+    }
+};
+
+template<>
+struct std::formatter<HumanResourcesEmployee>
+{
+    constexpr auto parse(std::format_parse_context &context) const
+    {
+        return context.begin();
+    }
+    
+    auto format(HumanResourcesEmployee const &employee, std::format_context &context) const
+    {
+        return std::format_to(context.out(),
+                "\nLogged in as Human Resources Employee"
+                "Please make a selection:\n"
+                "0. Exit\n"
+                "1. View employees.\n"
+                "2. Search for an employee.\n"
+                "3. Modify an employee.\n"
+                "4. Add an employee.\n"
+                "5. Remove an employee.\n"
+                );
+    }
+};
+
+template<>
+struct std::formatter<ManagerEmployee>
+{
+    constexpr auto parse(std::format_parse_context &context) const
+    {
+        return context.begin();
+    }
+    
+    auto format(ManagerEmployee const &employee, std::format_context &context) const
+    {
+        return std::format_to(context.out(),
+                "\nLogged in as Manager Employee"
+                "Please make a selection:\n"
+                "0. Exit\n"
+                "1. View employees.\n"
+                "2. Search for an employee.\n"
+                );
+    }
 };
 
 #endif
